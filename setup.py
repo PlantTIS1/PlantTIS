138b31b22188ad38bb15552ee51dd506a70c3e04
PlantTIS1 <82262372+PlantTIS1@users.noreply.github.com>
2021-04-10 19:08:11 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='PlantTISTool',
+      version='0.0.3',
+      description='clinical trial information retriver',
+      url='https://github.com/PlantTIS1/PlantTIS.git',
+      author='PlantTIS1',
+      author_email='1309985885@qq.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
